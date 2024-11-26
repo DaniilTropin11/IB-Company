addc76a107781bc114d8ad20d4eb21df3a30e6c2
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-11-26 08:59:42 +0700

    Добавьте файлы проекта.

---
+++ IB-Company/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using IB_Company.Data;
+using IB_Company.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IB_Company.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+			_db = db;
+        }
+
+
+        public IActionResult Index()
+		{
+			IEnumerable<Category> objlist = _db.Category; 
+			return View(objlist);
+		}
+	}
+}
