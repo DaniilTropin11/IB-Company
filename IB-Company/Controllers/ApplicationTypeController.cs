76677404a4b71b8b5dd65000a0c3ed99ce4a1a57
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-11-26 18:43:35 +0700

    Создание контроллера для типа (Application Type)

---
+++ IB-Company/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,39 @@
+﻿using IB_Company.Data;
+using IB_Company.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IB_Company.Controllers
+{
+	public class ApplicationTypeController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public ApplicationTypeController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+
+		public IActionResult Index()
+		{
+			IEnumerable<ApplicationType> objlist = _db.ApplicationType;
+			return View(objlist);
+		}
+		public IActionResult Create() // метод get Для операции create 
+		{
+
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj) // метод get Для операции create 
+		{
+			_db.ApplicationType.Add(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+        }
+    }
+}
