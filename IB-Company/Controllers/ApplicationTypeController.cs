e1172d125d5f52ec1d35c0a5fb7e5f2e1c9d62d2
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-11-28 18:43:09 +0700

    03-009 Добавление Валидаций для Application Type (на стороне клиента и сервера) +Добавление /удаление / редактирование

--- IB-Company/Controllers/ApplicationTypeController.cs
+++ IB-Company/Controllers/ApplicationTypeController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 
 namespace IB_Company.Controllers
 {
+	
+
 	public class ApplicationTypeController : Controller
 	{
 		private readonly ApplicationDbContext _db;
@@ -29,11 +31,78 @@ namespace IB_Company.Controllers
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-        public IActionResult Create(ApplicationType obj) // метод get Для операции create 
+		public IActionResult Create(ApplicationType obj) // метод get Для операции create 
 		{
-			_db.ApplicationType.Add(obj);
+			if (ModelState.IsValid) //валидация на стороне добавления 
+			{
+				_db.ApplicationType.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(obj);
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			return View(obj);
+		}
+
+		// метод get Для операции edit
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(ApplicationType obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.ApplicationType.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(obj);
+		}
+		///------------------------------------------------
+		public IActionResult Delete(int? id) // метод get Для операции delete
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			return View(obj);
+		}
+
+		// метод get Для операции delete
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.ApplicationType.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.ApplicationType.Remove(obj);
 			_db.SaveChanges();
 			return RedirectToAction("Index");
-        }
-    }
+
+		}
+
+	}
 }
--- IB-Company/Models/ApplicationType.cs
+++ IB-Company/Models/ApplicationType.cs
@@ -4,9 +4,10 @@ using System.ComponentModel.DataAnnotations;
 namespace IB_Company.Models
 {
 	public class ApplicationType
-    {
+	{
 		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; }
 		
 	}
