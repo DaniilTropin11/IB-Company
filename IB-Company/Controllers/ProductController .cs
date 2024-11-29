1990ea894384060130c904ab4a9b43ff0e365015
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-11-29 11:14:26 +0700

    04-003 Список товаров

---
+++ IB-Company/Controllers/ProductController .cs
@@ -0,0 +1,112 @@
+﻿using IB_Company.Data;
+using IB_Company.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IB_Company.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public ProductController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+
+		public IActionResult Index()
+		{
+			IEnumerable<Product> objlist = _db.Product;
+			foreach (var obj in objlist)
+			{
+				obj.Category = _db.Category.FirstOrDefault(u => u.Id== obj.CategoryId);
+			};
+			
+			return View(objlist);
+		}
+		public IActionResult Create() // метод get Для операции create 
+		{
+
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Category obj) // метод get Для операции create 
+		{
+			if (ModelState.IsValid) //валидация на стороне добавления 
+			{
+				_db.Category.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(obj);
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.Category.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			return View(obj);
+		}
+
+		// метод get Для операции edit
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Category.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(obj);
+		}
+		///------------------------------------------------
+		public IActionResult Delete(int? id) // метод get Для операции delete
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var obj = _db.Category.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			return View(obj);
+		}
+
+		// метод get Для операции delete
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.Category.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Category.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+
+		}
+
+	}
+}
