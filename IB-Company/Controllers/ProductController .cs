0395907dbcb2d105e972e577ba3459ccf7d78359
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-02 14:13:53 +0700

    004-014 Обновление изображения для Product

--- IB-Company/Controllers/ProductController .cs
+++ IB-Company/Controllers/ProductController .cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -107,7 +109,36 @@ namespace IB_Company.Controllers
 				else
 				{
 					//обновление 
+					var objFromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+					if (files.Count > 0)
+					{
+						{
+							string upload = webRootPath + WC.ImagePath;
+							string fileName = Guid.NewGuid().ToString();
+							string extension = Path.GetExtension(files[0].FileName);
+
+							var oldFile = Path.Combine(upload, objFromDb.Image);
+							if (System.IO.File.Exists(oldFile))
+							{
+								System.IO.File.Delete(oldFile);
+							}
+
+							using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+							{
+								files[0].CopyTo(fileStream);
+							}
+
+							productVM.Product.Image = fileName + extension;
+						}
+					}
+					else
+					{
+						productVM.Product.Image = objFromDb.Image;
+
+					}
+					_db.Product.Update(productVM.Product);
 				}
+
 				_db.SaveChanges();	
 				return RedirectToAction("Index");
 			}
