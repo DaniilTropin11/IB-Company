b85c5cf8592b6f4eeaeef42694e2195f838a8cca
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-05 13:30:37 +0700

    06-006 Регистарация Пользователя-Админа

--- IB-Company/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IB-Company/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,7 +92,7 @@ namespace IB_Company.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-
+                    await _userManager.AddToRoleAsync(user, WC.AdminRole);
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
