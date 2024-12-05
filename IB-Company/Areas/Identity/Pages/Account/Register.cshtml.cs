aa8b9b502f05d856a5cfa0dc6512f5c7c43e0f3e
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-05 13:25:42 +0700

    06-005 Настройка управления Ролями Добавление двух ключевых ролей в проекте в БД

--- IB-Company/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IB-Company/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -24,17 +24,21 @@ namespace IB_Company.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         [BindProperty]
@@ -68,6 +72,11 @@ namespace IB_Company.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (!await _roleManager.RoleExistsAsync(WC.AdminRole))       // создание ролей
+            {
+                await _roleManager.CreateAsync(new IdentityRole(WC.AdminRole));         // создание роли администратора 
+                await _roleManager.CreateAsync(new IdentityRole(WC.CustomerRole));      // создание роли клиента 
+            }
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }// обработчик функциональности при загрузки страницы 
@@ -83,6 +92,7 @@ namespace IB_Company.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
--- IB-Company/Startup.cs
+++ IB-Company/Startup.cs
@@ -30,7 +30,8 @@ namespace IB_Company
 			services.AddDbContext<ApplicationDbContext>(options =>
 			options.UseSqlServer(
 				Configuration.GetConnectionString("DefaultConnection")));
-			services.AddDefaultIdentity<IdentityUser>()
+			services.AddIdentity<IdentityUser,IdentityRole>()
+				.AddDefaultTokenProviders().AddDefaultUI()
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 
 			services.AddHttpContextAccessor();
