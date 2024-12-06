185137de479a92e830b84de1a0658ff36587473c
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-06 11:56:50 +0700

    07-005 Метод Get для Summary

--- IB-Company/Controllers/CartController.cs
+++ IB-Company/Controllers/CartController.cs
@@ -1,10 +1,12 @@
 ﻿using IB_Company.Data;
 using IB_Company.Models;
+using IB_Company.Models.ViewModels;
 using IB_Company.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace IB_Company.Controllers
 {
@@ -12,11 +14,13 @@ namespace IB_Company.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
         }
-        [Authorize]
+       
 
         public IActionResult Index()
         {
@@ -32,6 +36,40 @@ namespace IB_Company.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             
             return View(prodList);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+           
+
+            return RedirectToAction(nameof(Summary));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity; // если пользователь вошел в систему то объект будет получен 
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier); // если нет , объект не будет определен 
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // проверив эти условия , можно считать сессия существует и можно получить все товары и показать их 
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+
+            }
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+            };
+
+            return View(ProductUserVM);
         }
         public IActionResult Remove(int id)
         {
