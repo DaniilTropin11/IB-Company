fe2a9b3809af82c4892d030b39455886080d5db9
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-06 11:23:32 +0700

    07-003 Удаление предментов из Корзины

--- IB-Company/Controllers/CartController.cs
+++ IB-Company/Controllers/CartController.cs
@@ -30,5 +30,20 @@ namespace IB_Company.Controllers
             
             return View(prodList);
         }
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                // проверив эти условия , можно считать сессия существует и можно получить все товары и показать их 
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+
+            }
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
+            HttpContext.Session.Set(WC.SessionCart , shoppingCartList);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
