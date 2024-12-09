a74fa82384ec05a84230fb58c00401e9400d830a
DaniilTropin11 <112756589+DaniilTropin11@users.noreply.github.com>
2024-12-09 12:00:28 +0700

    07- 008,009,010,011,012 Пропустил данные реализации , т.к сервисы заблокированы (буду искать альтернативу) / примитивную реализацию отправки сообщения

---
+++ IB-Company/Utility/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace IB_Company.Utility // реализация почты для отправки заказов от клиента 
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+           
+        }   
+
+    }
+}
